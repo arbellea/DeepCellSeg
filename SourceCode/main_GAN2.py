fd7bc9c9d568d7190739050e5499ae3592b8a81d
arbellea <arbellea@post.bgu.ac.il>
2017-05-17 12:58:28 +0300

    REVERT BEFORE UNET

--- SourceCode/main_GAN2.py
+++ SourceCode/main_GAN2.py
@@ -163,7 +163,7 @@ class SegNetG(Network):
         # Layer 2
         kxy = 7
         kout = 32
-        conv = self.conv('conv2', relu, kxy, kxy, kout, kout, padding='VALID')
+        conv = self.conv('conv2', relu, kxy, kxy, kout, padding='VALID')
         bn = self.batch_norm('bn2', conv, phase_train, reuse)
         relu = self.leaky_relu('relu2', bn)
         crop_size += (kxy-1)/2
@@ -171,7 +171,7 @@ class SegNetG(Network):
         # Layer 3
         kxy = 5
         kout = 64
-        conv = self.conv('conv3', relu, kxy, kxy, kout, kout, padding='VALID')
+        conv = self.conv('conv3', relu, kxy, kxy, kout, padding='VALID')
         bn = self.batch_norm('bn3', conv, phase_train, reuse)
         relu = self.leaky_relu('relu3', bn)
         crop_size += (kxy-1)/2
@@ -179,7 +179,7 @@ class SegNetG(Network):
         # Layer 4
         kxy = 3
         kout = 64
-        conv = self.conv('conv4', relu, kxy, kxy, kout, kout, padding='VALID')
+        conv = self.conv('conv4', relu, kxy, kxy, kout, padding='VALID')
         bn = self.batch_norm('bn4', conv, phase_train, reuse)
         relu = self.leaky_relu('relu4', bn)
         crop_size += (kxy-1)/2
